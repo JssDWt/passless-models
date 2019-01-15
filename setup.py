1373fa85bc1b688f0e1d57db929baf9bab2681d8
Jesse de Wit <witdejesse@hotmail.com>
2019-01-15 10:06:49 +0100

    Moved source code to the 'src' directory for robustness

--- setup.py
+++ setup.py
@@ -1,4 +1,10 @@
-from setuptools import setup
+from setuptools import setup, find_packages
+from glob import glob
+from os.path import basename
+from os.path import dirname
+from os.path import join
+from os.path import relpath
+from os.path import splitext
 
 def readme():
     with open('README.rst') as f:
@@ -6,12 +12,15 @@ def readme():
 
 setup(
     name='passless_models',
-    version='0.0.1',
-    install_requires=['jsonpickle', 'python-dateutil'],
+    version='0.0.2',
     description='Models for Passless infrastructure.',
     long_description=readme(),
     author='Jesse de Wit',
     author_email='witdejesse@hotmail.com',
+    url='https://github.com/JssDWt/passless-models',
     license='GPLv3',
-    packages= ['passless_models']
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    py_modules=[splitext(basename(path))[0] for path in glob('src/*.py')],
+    install_requires=['jsonpickle', 'python-dateutil'],
 )
